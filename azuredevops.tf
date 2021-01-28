3b03fa53d6060bbf874bd5bd32d4c512607749b6
thegrew <Ali.Grew@cloudsolutions.co.uk>
2021-01-28 13:50:15 +0000

    Fix Azure Devops

--- azuredevops.tf
+++ azuredevops.tf
@@ -32,6 +32,7 @@ resource "azuredevops_variable_group" "azure_var_group" {
   variable {
     name         = "ENV_BLAISE_ADMIN_PASSWORD"
     secret_value = random_password.blaise_admin_password.result
+    is_secret    = true
   }
   variable {
     name  = "ENV_BLAISE_INTERNAL_SERVER_BINDING"
@@ -79,7 +80,8 @@ resource "azuredevops_variable_group" "azure_var_group" {
   }
   ##Below is Used by restAPI not CloudSQL Proxy
   variable {
-    name      = "ENV_DB_CONNECTIONSTRING"
-    value     = "User Id=${var.cloudsql_user};Server=localhost;Database=blaise;Password=${var.cloudsql_pw}"
+    name         = "ENV_DB_CONNECTIONSTRING"
+    secret_value = "User Id=${var.cloudsql_user};Server=localhost;Database=blaise;Password=${var.cloudsql_pw}"
+    is_secret    = true
   }
 }
